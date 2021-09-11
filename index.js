e304fa6a31ee867ee70da884807c08a75ddb655d
NoraElshahat <nelshohat@gmail.com>
2021-09-12 00:14:36 +0200

    save work for dashboard

---
+++ index.js
@@ -0,0 +1,140 @@
+const AdminBro = require('admin-bro');
+const AdminBroExpress = require('@admin-bro/express');
+const AdminBroMongoose = require('@admin-bro/mongoose');
+const mongoose = require('mongoose');
+AdminBro.registerAdapter(AdminBroMongoose);
+
+const express = require('express');
+const app = express();
+
+// career table
+const Careers = mongoose.model('Careers', {
+  type: {
+    type: Array,
+  },
+  name: { type: String },
+  email: { type: String },
+  subject: { type: String },
+  message: { type: String },
+});
+// media center
+const MediaCenter = mongoose.model('MediaCenter', {
+  title: {
+    type: String,
+  },
+  img: {
+    type: String,
+  },
+  href: {
+    type: String,
+  },
+});
+
+// news table
+const News = mongoose.model('News', {
+  title: {
+    type: String,
+    required: [true, 'Title is required'],
+  },
+  details: {
+    type: String,
+    required: [true, 'Desciption is required'],
+  },
+
+  date: {
+    type: String,
+  },
+  img: {
+    type: String,
+  },
+});
+
+// programs table
+const Programs = mongoose.model('Programs', {
+  title: {
+    type: String,
+  },
+  link: {
+    type: String,
+  },
+  details: {
+    type: Array,
+  },
+});
+// projects table
+const Projects = mongoose.model('Projects', {
+  title: {
+    type: String,
+    required: [true, 'title title required'],
+  },
+  img: {
+    type: String,
+  },
+  description: {
+    type: String,
+  },
+  objective: {
+    type: String,
+  },
+  trainingDetails: {
+    type: String,
+  },
+  sustainability: {
+    type: String,
+  },
+  pictures: [
+    {
+      type: String,
+    },
+  ],
+});
+// publications table
+const Publications = mongoose.model('Publications', {
+  img: { type: String },
+  title: { type: String },
+  link: { type: String },
+});
+
+// sectors table
+const Sectors = mongoose.model('Sectors', {
+  title: {
+    type: String,
+    required: [true, 'sector title required'],
+  },
+  img: {
+    type: String,
+  },
+  programs: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Programs',
+    },
+  ],
+});
+
+const run = async () => {
+  const mongooseDb = await mongoose.connect('mongodb://127.0.0.1:27017/sdf', {
+    useNewUrlParser: true,
+  });
+
+  // Passing resources by giving entire database
+  const adminBro = new AdminBro({
+    databases: [mongooseDb],
+    resources: [
+      Careers,
+      MediaCenter,
+      News,
+      Programs,
+      Projects,
+      Publications,
+      Sectors,
+    ],
+  });
+
+  const router = AdminBroExpress.buildRouter(adminBro);
+  app.use(adminBro.options.rootPath, router);
+};
+
+run();
+
+app.listen(8080, () => console.log('AdminBro is under localhost:8080/admin'));
---
+++ models/models.js
@@ -0,0 +1,17 @@
+const Sectors = require('../../sekem-development-foundation-backend/src/models/sectors');
+const MediaCenter = require('../../sekem-development-foundation-backend/src/models/media-center');
+const News = require('../../sekem-development-foundation-backend/src/models/news');
+const Programs = require('../../sekem-development-foundation-backend/src/models/programs');
+const Projects = require('../../sekem-development-foundation-backend/src/models/projects');
+const Publications = require('../../sekem-development-foundation-backend/src/models/publication');
+const Careers = require('../../sekem-development-foundation-backend/src/models/careers');
+
+module.exports = {
+  Sectors,
+  MediaCenter,
+  News,
+  Programs,
+  Projects,
+  Publications,
+  Careers,
+};
